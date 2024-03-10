52813498056ed25bb19d04eb535444e17ed51d57
kpbongat <kpbongat.dev@gmail.com>
2024-03-10 14:38:07 +0800

    Declare variables for operands and operation

--- js/script.js
+++ js/script.js
@@ -2,3 +2,20 @@ function add(num1,num2){
     return num1 + num2;
 }
 
+function subtract(num1,num2){
+    return num1 - num2;
+}
+
+function multiply(num1,num2){
+    return num1 * num2;
+}
+
+function divide(num1,num2){
+    return num1/num2;
+}
+
+let operandOne;
+let operandTwo;
+let operator;
+
+
